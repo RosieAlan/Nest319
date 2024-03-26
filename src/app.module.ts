55dc6a9d09d623a0f94ca3a1ef9ffc4d6be0df07
linjh <516308181@qq.com>
2024-03-26 17:05:49 +0800

    注册

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [],
+  imports: [ 
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "123456",
+      database: "meeting_room_booking_system",
+      synchronize: true,
+      logging: true,
+      entities: [
+        __dirname + '/**/*.entity{.ts,.js}',
+      ],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+          authPlugin: 'sha256_password',
+      }
+    }), UserModule, RedisModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
