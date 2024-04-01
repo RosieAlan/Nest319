09ab5b340429e44bb2c1b212dfb45e39b62631c0
linjh <516308181@qq.com>
2024-04-01 17:02:52 +0800

    鉴权

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,27 +4,50 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { RedisModule } from './redis/redis.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [ 
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "localhost",
-      port: 3306,
-      username: "root",
-      password: "123456",
-      database: "meeting_room_booking_system",
-      synchronize: true,
-      logging: true,
-      entities: [
-        __dirname + '/**/*.entity{.ts,.js}',
-      ],
-      poolSize: 10,
-      connectorPackage: 'mysql2',
-      extra: {
-          authPlugin: 'sha256_password',
-      }
-    }), UserModule, RedisModule,
+  imports: [
+    JwtModule.registerAsync({
+      global: true,
+      useFactory(configService: ConfigService) {
+        return {
+          secret: configService.get('jwt_secret'),
+          signOptions: {
+            expiresIn: '30m' // 默认 30 分钟
+          }
+        }
+      },
+      inject: [ConfigService]
+    }),
+    
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'src/.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory(configService: ConfigService) {
+        return {
+          type: 'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: 'root',
+          password: '123456',
+          database: 'meeting_room_booking_system',
+          synchronize: true,
+          logging: true,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          poolSize: 10,
+          connectorPackage: 'mysql2',
+          extra: {
+            authPlugin: 'sha256_password',
+          },
+        };
+      },
+    }),
+    UserModule,
+    RedisModule,
   ],
   controllers: [AppController],
   providers: [AppService],
